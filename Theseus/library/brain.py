2b2c7f3b21c77ba12121e3ac711b232a3605e195
Zia Hawwa <rziahawwa@gmail.com>
2026-09-01 20:26:56 +0530

    hmmm i need to learn context

--- Theseus/library/brain.py
+++ Theseus/library/brain.py
@@ -2,5 +2,5 @@
 brain = {
     "intro": "Hey, nice to meet you. I'm Theseus.",
     "casual": "Not much, just hanging out. How about you?",
-    "talk": "I'm doing well, thanks for asking. What about you?"
+    "talk": "I'm doing well, thanks for asking. What about you?",
 }
--- Theseus/matcher.py
+++ Theseus/matcher.py
@@ -2,7 +2,9 @@ from sanitizer import sanitize
 from library.patterns import patterns
 
 def pattern_matcher(sanitized_list):
+    if not sanitized_list:
+        return "unknown"
     for key in patterns:
-        if all(category in key for category in sanitized_list):
+        if all(category in sanitized_list for category in key):
             return patterns[key]
     return "unknown"

7c04eb147668a99670279863b9b7708a46dada0b
Zia Hawwa <rziahawwa@gmail.com>
2026-09-05 13:23:07 +0530

    more of a structured scraper

--- Theseus/command_helper/scraper.py
+++ Theseus/command_helper/scraper.py
@@ -1,4 +1,6 @@
 import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urlparse
 
 def scrape(url):
     headers = {
@@ -13,4 +15,51 @@ def scrape(url):
 
     response.raise_for_status()
 
-    return response.text
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    return extract_page_data(soup, url)
+
+def extract_page_data(soup, url):
+    title = (
+        soup.title.string.strip()
+        if soup.title and soup.title.string
+        else None
+    )
+
+    domain = urlparse(url).hostname
+
+    text = soup.get_text(" ", strip=True)
+
+    author = extract_author(soup)
+    date = extract_date(soup)
+
+    return {
+        "url": url,
+        "domain": domain,
+        "title": title,
+        "text": text,
+        "author": author,
+        "date": date
+    }
+
+def extract_author(soup):
+    author_tag = soup.find(
+        "meta",
+        attrs={"name": "author"}
+    )
+
+    if author_tag:
+        return author_tag.get("content")
+
+    return None
+
+def extract_date(soup):
+    date_tag = soup.find(
+        "meta",
+        attrs={"property": "article:published_time"}
+    )
+
+    if date_tag:
+        return date_tag.get("content")
+
+    return None

3f80e02a07e2ac5fe7389510fc618676bdf615cb
Zia Hawwa <rziahawwa@gmail.com>
2026-09-25 15:54:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from internet import Internet
+from identifier import Identifier
 
 def main() -> None:
     print("THESEUS")
@@ -11,19 +11,10 @@ def main() -> None:
         print("No target provided.")
         return
 
-    with Internet() as internet:
-        try:
-            page = internet.open(target)
+    identifier = Identifier()
+    entity = identifier.identify(target)
 
-            print()
-            print(f"URL:    {page.url}")
-            print(f"STATUS: {page.status}")
-            print(f"TITLE:  {page.title}")
-            print()
-            print(page.text)
-
-        except Exception as exc:
-            print(f"Error: {exc}")
+    print(entity)
 
 if __name__ == "__main__":
     main()
